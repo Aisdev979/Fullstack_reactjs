4665cffa6adff93ba3db00ade3688c617e0ff76e
Ogbebor Aisosa <aisdevinstitute@gmail.com>
2025-12-22 06:52:47 +0100

    Deploying my app

--- 02_password_generator/password-generator/vite.config.js
+++ 02_password_generator/password-generator/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fullstack_reactjs/02_password_generator/password-generator/dist/'
 })
